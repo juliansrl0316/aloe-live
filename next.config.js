94c5ec3f38cbfbfa911de5816b1c4edac1795b57
juliansrl0316 <julian09112001@gmail.com>
2022-12-12 01:20:14 -0500

    CORS

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ["links.papareact.com", "fakestoreapi.com", "http2.mlstatic.com", "centrosthetic.com.co", "qhubocali.com", "i.pinimg.com", "www.qhubocali.com", "www.digitalmenta.com", "img.freepik.com", "https://res.cloudinary.com/", "freesvg.org" ],
+    domains: ["links.papareact.com", "fakestoreapi.com", "http2.mlstatic.com", "centrosthetic.com.co", "qhubocali.com", "i.pinimg.com", "qhubocali.com", "digitalmenta.com", "img.freepik.com", "https://res.cloudinary.com/", "freesvg.org"],
   },
   env: {
     stripe_public_key: "pk_test_51Ko9jPJG12VEnZfiEzS4OvLwbuJZElF4fxGlA2BA5VqzJDYSa16WYgNUfdk5Z30qmRzQvOeJNlKbFBMSu54pIPM000iIGwN9P7",
